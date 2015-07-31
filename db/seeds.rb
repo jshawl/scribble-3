cb9671b3d27b8a0af2201219db4ba2ec4633428c
Anand Moses <moses.adg@gmail.com>
2015-07-30 20:12:45 -0400

    completed models and migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# Test comment
 
 Post.destroy_all
 Comment.destroy_all
